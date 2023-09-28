0dd8064c73d935167ad868a9ede54a425d4e07ea
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-09-28 23:41:13 +0100

    Downloaded and installed my fonts

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import "../App.css";
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <>
+      <div className="font-clashfont">AboutUs</div>
+      <div className="rounded-lg relative left-[868px] bg-slate-300 h-[315px] w-[491px]"></div>
+    </>
+  );
 };
 
 export default AboutUs;
