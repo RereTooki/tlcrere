89208066be834b8337e8d5b09ea7ec90cb403048
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-09-29 09:03:13 +0100

    Made my design responsive

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -4,8 +4,27 @@ import "../App.css";
 const AboutUs = () => {
   return (
     <>
-      <div className="font-clashfont">AboutUs</div>
-      <div className="rounded-lg relative left-[868px] bg-slate-300 h-[315px] w-[491px]"></div>
+      <div className="font-clashfont text-[55px] ml-[36.96px] mr-[20.8px] text-dark-black">
+        About The label Clan
+      </div>
+      <div className="relative">
+        <div className="absolute right-[28px] bottom-[88px] border-t-[1.4px] w-[32vw] border-black"></div>
+      </div>
+      <div className="font-roboto leading-[32px] mt-[55.27px] ml-[36px] mr-[37.61px] pb-[24.15px] mb-[93.99px]">
+        Great software that allows you to chat from any place at any time
+        without any nterruption. Great software that allows you to chat from any
+        place at any time without any nterruption.
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="ml-[21vw] bg-slate-300 h-[280px] w-[320px]"></div>
+        <div className="relative mr-[19vw] bottom-[44px] bg-slate-700 h-[315px] w-[328px]"></div>
+      </div>
+      {/* <div className="relative">
+        <div className="absolute top-[600px] right-[30px] bg-slate-300 h-[280px] w-[320px]"></div>
+        <div className="absolute top-[836px] right-[82px] tsop-[236px] bg-slate-700 h-[315px] w-[328px]"></div>
+      </div> */}
+
+      {/* <div className="rounded-lg relative left-[868px] bg-slate-300 h-[315px] w-[491px]"></div> */}
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ export default {
       fontFamily: {
         roboto: ['Roboto'],
         clashfont: ['clashdisplay']
- } 
+      },
+
+      colors: {
+        'orangee': 'hsl(25, 97%, 53%)',
+        'dark-black': 'hsla(255, 7%, 11%, 87%)',
+        'whitee': 'hsl(0, 0%, 100%)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'medium-grey': 'hsl(216, 12%, 54%)',
+        'dark-blue': 'hsl(213, 19%, 18%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)',
+      }, 
     },
   },
   plugins: [],
