7162cfd705d88eabb718addb0b5b458afb1106e9
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-09-29 10:07:23 +0100

    About to make some big changes

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -19,6 +19,31 @@ const AboutUs = () => {
         <div className="ml-[21vw] bg-slate-300 h-[280px] w-[320px]"></div>
         <div className="relative mr-[19vw] bottom-[44px] bg-slate-700 h-[315px] w-[328px]"></div>
       </div>
+      <div className="mt-[110.51px] flex flex-row justify-evenly mb-[76px]">
+        <div className="bg-slate-300 h-[315px] w-[238px]"></div>
+        <div className="relative top-[157.5px] border-t-[1.4px] w-[26vw] border-black"></div>
+      </div>
+      <div className="ml-[33px] mb-[127px]">
+        <div className="font-clashfont text-[55px] text-dark-black">
+          OUR STORY
+        </div>
+        <div className="relative top-[s157.5px] border-t-[1.4px] w-[186px] border-black"></div>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="ml-[14vw] bg-slate-300 h-[260px] w-[374px]"></div>
+        <div className="relative mr-[9.2vw] bottom-[210px] bg-slate-700 h-[303px] w-[395px]"></div>
+      </div>
+      <div className="relative bosttom-[134px]  font-roboto leading-[32px] ml-[36px] mr-[37.61px]">
+        Great software that allows you to chat from any place at any time
+        without any nterruption. Great software that allows you to chat from any
+        place at any time without any nterruption.
+      </div>
+      <div className="font-roboto leading-[32px] mts-[155.27px] ml-[36px] mr-[37.61px] pb-[24.15px] mb-[93.99px]">
+        Great software that allows you to chat from any place at any time
+        without any nterruption. Great software that allows you to chat from any
+        place at any time without any nterruption.
+      </div>
+
       {/* <div className="relative">
         <div className="absolute top-[600px] right-[30px] bg-slate-300 h-[280px] w-[320px]"></div>
         <div className="absolute top-[836px] right-[82px] tsop-[236px] bg-slate-700 h-[315px] w-[328px]"></div>
